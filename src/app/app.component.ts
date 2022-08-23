b8461307892c1118ea151e46fd39053b05169aea
Moises Saez <101760304+Msaez9@users.noreply.github.com>
2022-08-22 20:14:38 -0400

    cambio en el packege por autoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,8 @@ export class AppComponent {
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+  public ListaDeCosas = ['perros','autos','futbol','playa']
+
   constructor() {}
 }
